10f59a453932e703fe820e9088fe2e62da3f9171
Shineuiseong <crossstory12@gmail.com>
2022-03-27 17:04:54 +0900

    client ADD Resume

--- client/src/components/Resume/Resume.tsx
+++ client/src/components/Resume/Resume.tsx
@@ -5,18 +5,31 @@ import Animations from 'util/Animations'
 import { programmingSkillsDetails, projectDetails, resumeBullets } from './ResumeSkill'
 import './Resume.css'
 
-const Resume = (props) => {
-  const [selectedBulletIndex, setSelectedBulletIndex] = useState(0)
-  const [carousalOffsetStyle, setCarousalOffsetStyle] = useState({})
 
-  let fadeInScreenHandler = (screen) => {
+type ResumeProps={
+
+  fadeInScreen:string |undefined
+  id:string |undefined
+  heading:string |undefined
+  fromDate:string |undefined
+  toDate:string |undefined
+  subHeading:string |undefined
+  description:string |undefined
+
+}
+
+const Resume = (props:ResumeProps) => {
+  const [selectedBulletIndex, setSelectedBulletIndex] = useState(0)
+  const [carousalOffsetStyle, setCarousalOffsetStyle] = useState<any>({})
+
+  let fadeInScreenHandler = (screen:ResumeProps) => {
     if (screen.fadeInScreen !== props.id) return
     Animations.animations.fadeInScreen(props.id)
   }
 
-  const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+  ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
 
-  const ResumeHeading = (props) => {
+  const ResumeHeading = (props:ResumeProps) => {
     return (
       <div className="resume-heading">
         <div className="resume-main-heading">
@@ -44,46 +57,61 @@ const Resume = (props) => {
         heading={'배재대학교 게임공학과'}
         subHeading={'4년제 학사 졸업'}
         fromDate={'2013'}
-        toDate={'2019'}
-      />
+        toDate={'2019'} fadeInScreen={undefined} id={undefined} description={undefined}      />
     </div>,
-    /* WORK EXPERIENCE */
+    /* 경력 */
     <div className="resume-screen-container" key="work-experience">
       <div className="experience-container">
         <ResumeHeading
           heading={'(주)세오'}
           subHeading={'제품개발인증팀 연구원'}
           fromDate={'2020.04'}
-          toDate={'2021.12.31'}
-        />
+          toDate={'2021.12'} fadeInScreen={undefined} id={undefined} description={undefined}        />
         <div className="experience-description">
           <span className="resume-description-text">
-            Currently working as E&P Data Specialist handling E&P technical database and data integration development.
+            - 2020.04 ~ 2020.08 암호화 영상 및 복호화 영상 프로세스 관리 시스템 개발 건
           </span>
         </div>
         <div className="experience-description">
-          <span className="resume-description-text">
-            - Developed end-to-end data management system for G&G technical data using GIS Framework and Application
+          <span className="resume-description-text-camera">
+            - 2020.08 ~ 2021.10 다차선 번호인식 통합형 무인교통 단속시스템 개발 건 (RS232 레이더 정보 컨트롤, 단속
+            시스템 및 모니터링 관제 부분 개발)
           </span>
           <br />
+        </div>
+        <div className="experience-description">
           <span className="resume-description-text">
-            - Developed API and real-time monitoring for production and facility data.
+            - 2021.09 ~ 2021.10 소프트웨어 GS인증평가 무인교통 단속시스템 진행완료
           </span>
           <br />
-          <span className="resume-description-text">
-            - Developed ETL tools to digitize multi-document format into semi-automated services and database.
-          </span>
+        </div>
+        <div className="experience-description">
+          <span className="resume-description-text">- 2021.09 ~ 2021.12 한국 기게연구원 차량 관리 시스템 개발</span>
+          <br />
+        </div>
+        <br />
+        <ResumeHeading
+          heading={'(주)스태프프로젝트'}
+          subHeading={'개발 연구원'}
+          fromDate={'2019.03'}
+          toDate={'2020.04'} fadeInScreen={undefined} id={undefined} description={undefined}        />
+        <div className="experience-description">
+          <span className="resume-description-text">- 당뇨병 치료용 센서 데이터 수집 어플 개발 (Android,Arduino)</span>
           <br />
         </div>
       </div>
     </div>,
 
-    /* PROGRAMMING SKILLS */
+    /* 스킬 */
     <div className="resume-screen-container programming-skills-container" key="programming-skills">
       {programmingSkillsDetails.map((skill, index) => (
         <div className="skill-parent" key={index}>
           <div className="heading-bullet"></div>
-          <span>{skill.skill}</span>
+          <span className="heading-bullet-skill">
+            {skill.skill}
+            <span className="heading-bullet-skill-percentage"> {skill.ratingPercentage / 10}/10</span>
+          </span>
+
           <div className="skill-percentage">
             <div style={{ width: skill.ratingPercentage + '%' }} className="active-percentage-bar"></div>
           </div>
@@ -91,7 +119,7 @@ const Resume = (props) => {
       ))}
     </div>,
 
-    /* PROJECTS */
+    /* 프로젝트 */
     <div className="resume-screen-container" key="projects">
       {projectDetails.map((projectsDetails, index) => (
         <ResumeHeading
@@ -100,26 +128,12 @@ const Resume = (props) => {
           subHeading={projectsDetails.subHeading}
           description={projectsDetails.description}
           fromDate={projectsDetails.duration.fromDate}
-          toDate={projectsDetails.duration.toDate}
-        />
+          toDate={projectsDetails.duration.toDate} fadeInScreen={undefined} id={undefined}        />
       ))}
-    </div>,
-
-    /* Interests */
-    <div className="resume-screen-container" key="interests">
-      <ResumeHeading heading="Sports" description="Like to cycling, basketball and swimming." />
-      <ResumeHeading
-        heading="Hydroponics"
-        description="Build my own hydroponics system and step-by-step understanding the process."
-      />
-      <ResumeHeading
-        heading="Investment"
-        description="Started to invest in farming area include ducks and sheep also interest in stock market investment"
-      />
     </div>,
   ]
 
-  const handleCarousal = (index) => {
+  const handleCarousal = (index:any) => {
     let offsetHeight = 360
     let newCarousalOffset = {
       style: { transform: 'translateY(' + index * offsetHeight * -1 + 'px)' },
--- client/src/components/Resume/ResumeSkill.ts
+++ client/src/components/Resume/ResumeSkill.ts
@@ -2,10 +2,10 @@
 export const resumeBullets = [
     { label: '학력', logoSrc: 'education.svg' },
     { label: '경력', logoSrc: 'work-history.svg' },
-    { label: 'Programming Skills', logoSrc: 'programming-skills.svg' },
+    { label: '스킬', logoSrc: 'programming-skills.svg' },
     // { label: 'Application Skills', logoSrc: 'programming-skills.svg' },
-    { label: 'Projects', logoSrc: 'projects.svg' },
-    { label: 'Interests', logoSrc: 'interests.svg' },
+    { label: '프로젝트', logoSrc: 'projects.svg' },
+    
   ]
 
 export const programmingSkillsDetails = [
@@ -24,9 +24,30 @@ export const programmingSkillsDetails = [
 export const projectDetails = [
     {
         title:'Portfolio Website',
-        duration: { fromDate: "2022", toDate: "2022" },
+        duration: { fromDate: "2022.03", toDate: "2022.03" },
         description:
         "개인 포트폴리오 웹사이트 입니다. 자기소개 및 스킬, 프로젝트를 소개합니다.",
         subHeading: "Technologies Used: React JS, Bootstrap",
+    },
+    {
+        title:'Bolierplate_React',
+        duration: { fromDate: "2022.02", toDate: "2022.02" },
+        description:
+        "소셜 로그인, 닉네임설정, 마이페이지등 개인 프로젝트에 기능을 재사용하기위한 보일러플레이트 입니다.",
+        subHeading: "Technologies Used: React JS, NodeJS Express, MongoDB",
+    },
+    {
+        title:'Movie Search WebSite',
+        duration: { fromDate: "2022.02", toDate: "2022.02" },
+        description:
+        "oMDb api를 사용하여 영화 데이터를 요청하고 구성된 레이아웃에 출력하는 프로젝트입니다.",
+        subHeading: "Technologies Used: VueJS ,OMDb API ",
+    },
+    {
+        title:'Blind Clone Website',
+        duration: { fromDate: "2022.01", toDate: "2022.02" },
+        description:
+        "Blind Website clone 프로젝트입니다. ",
+        subHeading: "Technologies Used: VueJS , NuxtJs, NodeJS Express, MongoDB",
     }
 ]
